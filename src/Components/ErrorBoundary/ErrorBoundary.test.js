77ea82cdae1d680c45f9308bd56cba18d6c44b72
Jack Lansing <john.a.lansing@gmail.com>
2020-03-13 16:51:40 -0400

    fixed error boundary smoke test

--- src/Components/ErrorBoundary/ErrorBoundary.test.js
+++ src/Components/ErrorBoundary/ErrorBoundary.test.js
@@ -4,6 +4,11 @@ import ErrorBoundary from './ErrorBoundary';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<ErrorBoundary />, div);
+  ReactDOM.render(
+    <ErrorBoundary>
+      <></>
+    </ErrorBoundary>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
